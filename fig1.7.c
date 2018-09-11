0343e55721e13a0b264c9b0953c72dad175ba8fa
harkhuang <shiyanhk@gmail.com>
2018-09-11 16:36:44 +0800

    a

--- fig1.7.c
+++ fig1.7.c
@@ -1,28 +1,31 @@
 #include "apue.h"
 #include <sys/wait.h>
-
+#include <sysexits.h>
 int
 main(void)
 {
-	char buf[MAXLINE];
-	pid_t pid;
-	int status;
+	char	buf[MAXLINE];	/* from apue.h */
+	pid_t	pid;
+	int		status;
 
-	printf("%% ");
-	while (fgets(buf, MAXLINE, stdin) != NULL){
-		if(buf[strlen(buf) - 1] == '\n')
-		   buf[strlen(buf) - 1] = 0;
-		
-		if((pid = fork() < 0)){
-			err_sys("fork erro");
-		} else if (pid == 0 ){ //chilid
-			execlp(buf,buf,(char *) 0);
-			err_ret("could't execute:%s",buf);
-			exit(127);
+	printf("%% ");	/* print prompt (printf requires %% to print %) */
+	while (fgets(buf, MAXLINE, stdin) != NULL) {
+		if (buf[strlen(buf) - 1] == '\n')
+		   buf[strlen(buf) - 1] = 0; /* replace newline with null */
+
+		// 此处出错排查很久  因为  1&0仍然是真  程序输出forkerror
+		if ((pid = fork()) < 0) {
+			err_sys("fork error");
+		} else if (pid == 0) {		/* child */
+			execlp(buf, buf, (char *)0);
+			err_ret("couldn't execute: %s", buf);
+			exit(127); // 127自定义的一个返回值
 		}
-		if((pid = waitpid(pid, &status, 0)) < 0)
+		
+		
+			if ((pid = waitpid(pid, &status, 0)) < 0)
 			err_sys("waitpid error");
-		printf("%%");
+		printf("%% ");
 
 	}
 	exit(0);
