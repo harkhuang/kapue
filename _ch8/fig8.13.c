fd14ffb57e9564eaadff497c8670509fafa7360c
harkhuang <shiyanhk@gmail.com>
2018-09-18 12:46:36 +0800

    mod _ch8

---
+++ _ch8/fig8.13.c
@@ -0,0 +1,36 @@
+#include "apue.h"
+
+static void charatatime(char *);
+
+int
+main(void)
+{
+	pid_t	pid;
+
+	// tell wait do what ?
+	// how to fix the sync after fork?
+	
+	TELL_WAIT();
+
+	if ((pid = fork()) < 0) {
+		err_sys("fork error");
+	} else if (pid == 0) {
+		WAIT_PARENT();		/* parent goes first */
+		charatatime("output from child\n");
+	} else {
+		charatatime("output from parent\n");
+		TELL_CHILD(pid);
+	}
+	exit(0);
+}
+
+static void
+charatatime(char *str)
+{
+	char	*ptr;
+	int		c;
+
+	setbuf(stdout, NULL);			/* set unbuffered */
+	for (ptr = str; (c = *ptr++) != 0; )
+		putc(c, stdout);
+}
