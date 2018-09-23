68baf5ea6f796f2e18e9e85a8b160a759dc71261
harkhuang <shiyanhk@gmail.com>
2018-09-23 22:58:30 +0800

    线程切换如果,资源获取,线程竞争

--- _ch11/fig11.5.c
+++ _ch11/fig11.5.c
@@ -11,6 +11,7 @@ void *
 thr_fn1(void *arg)
 {
 	printf("thread 1 start\n");
+	// 将cleanup压栈,然后pop出栈调用
 	pthread_cleanup_push(cleanup, "thread 1 first handler");
 	pthread_cleanup_push(cleanup, "thread 1 second handler");
 	printf("thread 1 push complete\n");
@@ -25,6 +26,7 @@ void *
 thr_fn2(void *arg)
 {
 	printf("thread 2 start\n");
+	// 
 	pthread_cleanup_push(cleanup, "thread 2 first handler");
 	pthread_cleanup_push(cleanup, "thread 2 second handler");
 	printf("thread 2 push complete\n");
@@ -51,6 +53,9 @@ main(void)
 	err = pthread_join(tid1, &tret);
 	if (err != 0)
 		err_quit("can't join with thread 1: %s\n", strerror(err));
+
+
+	printf("-------------------\n");	
 	printf("thread 1 exit code %d\n", (int)tret);
 	err = pthread_join(tid2, &tret);
 	if (err != 0)
